ed1754d496eac78c4d7dd3d786d9817e944d0901
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-15 14:35:12 +0100

    Task 0 - Betty quizz Task 1 - Added files + proto in sort.h

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements of an array
+ * @a: Integer a
+ * @b: Integer b
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in
+ * ascending order
+ * @list: List to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+}
