52da60b41fb248d991f84023acc97a485e4a6d88
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-15 12:49:55 +0100

    Pushing README.md and given ressources

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Doubly linked list node
+ * @array: Array of integers to print
+ * @size: Size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
