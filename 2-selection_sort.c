0632c8a7c778c36aa6cc8ce8d7b1e38876b93854
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-15 15:33:54 +0100

    Task 2 - Locally tested : OK

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements of an array
+ * @a: Integer a
+ * @b: Integer b
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * selection_sort - sorts an array of integers in ascending order using
+ * the Selection sort algorithm
+ * @array: Array of integers
+ * @size: Size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			swap(&array[i], &array[min_idx]);
+			print_array(array, size);
+		}
+	}
+}
