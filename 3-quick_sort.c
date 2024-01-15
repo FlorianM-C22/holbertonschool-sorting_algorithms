86dc26a038f7b38f28386f532fb45dabd1546627
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-15 15:47:25 +0100

    Task 3 - Added initial files

---
+++ 3-quick_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements of an array
+ * @a: Integer a
+ * @b: Integer b
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm
+ * @array: Array of integers
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+
+}
